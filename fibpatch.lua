9c0de7174b7d35b6b4e7f7ec374f492c5dd6be4e
Jan Gabrielsson <jan@gabrielsson.com>
2025-03-06 11:35:41 +0100

    new

--- fibpatch.lua
+++ fibpatch.lua
@@ -21,8 +21,23 @@ function QuickApp:getQA(user,repo,name,tag,cb)
 end
 
 function QuickApp:getQAReleases(user,repo,name,cb)
+  local name = name:match("(.+)%.fqa") or name
+  name = name..".releases"
   local url = fmt("https://raw.githubusercontent.com/%s/%s/master/%s",user,repo,name..".rel")
   net.HTTPClient():get(url,{
+    options = {checkCertificate = false, timeout=20000},
+    success = function(response)
+      if response and response.status == 200 then
+        cb(true,response.data)
+      else cb(false,response and response.status or "nil") end
+    end,
+    error = function(err) cb(false,err) end
+  })
+end
+
+function QuickApp:getQATags(user,repo,cb)
+  local url = fmt("https://api.github.com/repos/%s/%s/tags",user,repo)
+  net.HTTPClient():request(url,{
     options = {checkCertificate = false, timeout=20000},
     success = function(response)
       if response and response.status == 200 then
@@ -39,5 +54,14 @@ function QuickApp:onInit()
     fibaro.hc3emu.loadQA("test/QA_A.lua")
   end
 
-  self:post({type='start'})
+  self:getQATags("jangabrielsson","fibpatch",function(ok,data)
+    if ok then
+      local tags = json.decode(data)
+      for i,tag in ipairs(tags) do
+        self:debug(tag.name)
+      end
+    else
+      self:debug(data)
+    end
+  end)
 end
